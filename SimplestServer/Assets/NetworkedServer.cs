3d7b4280d0721768cb642e31656deab93e085814
lazynorie <chengjy1991@gmail.com>
2021-12-06 14:56:32 -0500

    added signifiers

--- SimplestServer/Assets/NetworkedServer.cs
+++ SimplestServer/Assets/NetworkedServer.cs
@@ -22,6 +22,8 @@ public class NetworkedServer : MonoBehaviour
 
     private LinkedList<GameSession> _gameSessions;
 
+    private GameSessionManager _gameSessionManager;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -40,6 +42,10 @@ public class NetworkedServer : MonoBehaviour
         
         playerAccounts = new LinkedList<PlayerAccount>();
         _gameSessions = new LinkedList<GameSession>();
+        _gameSessionManager = new GameSessionManager();
+        
+        LoadPlayerAccounts();
+        
     }
 
     // Update is called once per frame
@@ -166,29 +172,22 @@ public class NetworkedServer : MonoBehaviour
 
             if (playerWaitingForMatch == -1)
             {
-                //make a single int variable to represent the one and only possible wating player
                 playerWaitingForMatch = id;
             }
-            else //if  there is a waiting player , join
+            else
             {
-                //START GAME SESSION
-                //create the game session object, pass it to two players
-                //beginning of piping
-                GameSession gs = new GameSession(playerWaitingForMatch, id);
-                _gameSessions.AddLast(gs);
+                int gameroomID = GameSessionManager.GetGameSessionIDnumber();
+                GameSession gs = new GameSession(playerWaitingForMatch, id, gameroomID);
+                //_gameSessions.AddLast(gs);
+                _gameSessionManager.allGameSession.Add(gs);
                 
-                //pass siginifier to both clients that they've joined one
-                // SendMessageToClient(ServerToClientSignifiers.GameSessionStarted + "", id);
-                // SendMessageToClient(ServerToClientSignifiers.GameSessionStarted + "", playerWaitingForMatch);
 
                 int playerWaitingForMatchMovesFirst = Random.Range(0, 2);
                 int currentPlayersMove = (playerWaitingForMatchMovesFirst == 1)?0:1;
+                //[0]signifier [1]who moves first [2]gameroomID
+                SendMessageToClient(string.Join(",",ServerToClientSignifiers.GameSessionStarted.ToString(), playerWaitingForMatchMovesFirst,gameroomID.ToString()), playerWaitingForMatch);
+                SendMessageToClient(string.Join(",",ServerToClientSignifiers.GameSessionStarted.ToString(), currentPlayersMove,gameroomID.ToString()), id);
                 
-                SendMessageToClient(string.Join(",",ServerToClientSignifiers.GameSessionStarted.ToString(), playerWaitingForMatchMovesFirst), playerWaitingForMatch);
-                SendMessageToClient(string.Join(",", ServerToClientSignifiers.GameSessionStarted.ToString(), currentPlayersMove) + "", id);
-
-                
-                //reset game matching queue
                 playerWaitingForMatch = -1;
             }
         }
@@ -208,15 +207,22 @@ public class NetworkedServer : MonoBehaviour
         {
             //csv[0]siginifier  csv[1]which button   csv[2]change my turn  csv[3]tell server it's opponent's turn
             GameSession gs = FindGameSessionWithPlayerID(id);
+            if (gs != null)
+            {
+                if (gs.playerID1 ==id)
+                {
+                    SendMessageToClient(string.Join(",",ServerToClientSignifiers.OpponentTicTacToePlay.ToString(),csv[1],csv[2],csv[3]),gs.playerID2);
+                }
+                else
+                {
+                    SendMessageToClient(string.Join(",",ServerToClientSignifiers.OpponentTicTacToePlay.ToString(),csv[1],csv[2],csv[3]),gs.playerID1); 
+                }
+                foreach (int observerID in gs.observerIDs)
+                {
+                    SendMessageToClient(string.Join(",", ServerToClientSignifiers.UpDateOB.ToString(), csv[1], csv[2],csv[3]), observerID);
+                }
+            }
             
-            if (gs.playerID1 ==id)
-            {
-                SendMessageToClient(string.Join(",",ServerToClientSignifiers.OpponentTicTacToePlay.ToString(),csv[1],csv[2],csv[3]),gs.playerID2);
-            }
-            else
-            {
-                SendMessageToClient(string.Join(",",ServerToClientSignifiers.OpponentTicTacToePlay.ToString(),csv[1],csv[2],csv[3]),gs.playerID1);
-            }
         }
         else if (singifier == ClientToServerSignifiers.PlayerMessage)
         {
@@ -231,16 +237,29 @@ public class NetworkedServer : MonoBehaviour
                 {
                     SendMessageToClient(string.Join(",",ServerToClientSignifiers.SendChatToOpponent.ToString(),csv[1],csv[2]),gs.playerID1);
                 }
+                foreach (int observerNum in gs.observerIDs)
+                {
+                    if (observerNum != id)
+                        SendMessageToClient(string.Join(",", ServerToClientSignifiers.SendChatToOpponent.ToString(), csv[1], csv[2]), observerNum);
+                }
             }
-            // else
-            // {
-            //     gs = FindGameSessionWithPlayerID(id);
-            //     if (gs!=null)
-            //     {
-            //         SendMessageToClient(string.Join(",", ServerToClientSignifiers.SendChatToOpponent.ToString(), csv[1], csv[2]), gs.playerID1);
-            //         SendMessageToClient(string.Join(",", ServerToClientSignifiers.SendChatToOpponent.ToString(), csv[1], csv[2]), gs.playerID2);
-            //     }
-            // }
+            else
+            {
+                gs = FindGameSessionWithObserverID(id);
+                if (gs != null)
+                {
+                    SendMessageToClient(string.Join(",", ServerToClientSignifiers.SendChatToOpponent.ToString(), csv[1], csv[2]), gs.playerID1);
+                    SendMessageToClient(string.Join(",", ServerToClientSignifiers.SendChatToOpponent.ToString(), csv[1], csv[2]), gs.playerID2);
+
+                    foreach(int observerNum in gs.observerIDs)
+                    {
+                        if (observerNum != id)
+                            SendMessageToClient(string.Join(",", ServerToClientSignifiers.SendChatToOpponent.ToString(), csv[1], csv[2]), observerNum);
+
+                    }
+                }
+
+            }
         }
         else if (singifier == ClientToServerSignifiers.WinMsg)
         {
@@ -257,6 +276,34 @@ public class NetworkedServer : MonoBehaviour
                 }
             }
         }
+        else if (singifier == ClientToServerSignifiers.TicTacToePlay)
+        {
+            int GameRoomID = int.Parse(csv[1]);
+            foreach (GameSession gameSession in _gameSessionManager.allGameSession)
+            {
+                if (GameRoomID == gameSession.roomID)
+                {
+                    SendMessageToClient(string.Join(",",ServerToClientSignifiers.OBrequestRecieved),id);
+                    gameSession.observerIDs.Add(id);
+                }
+            }
+        }
+        else if (singifier == ClientToServerSignifiers.OBrequestSent)
+        {
+            int GameRoomID = int.Parse(csv[1]);
+            foreach (GameSession gs in _gameSessionManager.allGameSession)
+            {
+                if (GameRoomID == gs.roomID)
+                {
+                    SendMessageToClient(string.Join(",",ServerToClientSignifiers.OBrequestRecieved),id);
+                    //to get 
+                    SendMessageToClient(string.Join(",",ServerToClientSignifiers.UpdateCurrentBoardToOB.ToString(),id.ToString()),gs.playerID1);
+                    gs.observerIDs.Add(id);
+                    return;
+                }
+            }
+        }
+       
     }
     
     private void SavePlayerAccounts()
@@ -290,10 +337,24 @@ public class NetworkedServer : MonoBehaviour
 
     private GameSession FindGameSessionWithPlayerID(int id)
     {
-        foreach (GameSession gs in _gameSessions)
+        foreach (GameSession gs in _gameSessionManager.allGameSession)
         {
             if (gs.playerID1 == id || gs.playerID2 == id)
                 return gs;
+        }
+
+        return null;
+    }
+
+    private GameSession FindGameSessionWithObserverID(int id)
+    {
+        foreach (GameSession gs in _gameSessionManager.allGameSession)
+        {
+            foreach (int observerID in gs.observerIDs)
+            {
+                if (observerID == id)
+                    return gs;
+            }
         }
 
         return null;
@@ -304,17 +365,41 @@ public class NetworkedServer : MonoBehaviour
 
 public class GameSession
 {
-    public int playerID1, playerID2;
+    public int playerID1, playerID2, roomID;
 
-    public GameSession(int PlayerID1, int PlayerID2)
+    public List<int> observerIDs;
+    
+    public GameSession(int PlayerID1, int PlayerID2, int RoomID)
     {
         playerID1 = PlayerID1;
         playerID2 = PlayerID2;
+        roomID = RoomID;
+
+        observerIDs = new List<int>();
     }
     //Hold two clients
     //to do work item
     //... but we are working to do it, with plan of coming back once we have a better understanding of whats going on and what we must do
 }
+
+public class GameSessionManager
+{
+    public static int nextGameSessionIDNumber = 0;
+    public static int GetGameSessionIDnumber()
+    {
+        nextGameSessionIDNumber++;
+        return nextGameSessionIDNumber;
+    }
+    public List<GameSession> allGameSession;
+    public GameSessionManager()
+    {
+        allGameSession = new List<GameSession>();
+    }
+}
+
+
+
+
 //set up account class
 public class PlayerAccount
 {
@@ -336,23 +421,23 @@ public static class ClientToServerSignifiers
     public const int PlayerMessage = 5;
     public const int TicTacToePlayMade = 6;
     public const int WinMsg = 7;
+    public const int OBrequestSent = 8;
+   
     
+
 }
 
 public static class ServerToClientSignifiers
 {
     public const int LoginResponse = 1;
-
     public const int GameSessionStarted = 2;
-    
     public const int OpponentTicTacToePlay = 3;
-
     public const int SendChatToOpponent = 4;
-
     public const int PlayerDC = 5;
-
     public const int GGMsg = 6;
-
+    public const int OBrequestRecieved = 8;
+    public const int UpdateCurrentBoardToOB = 9;
+    public const int UpDateOB = 10;
 }
 
 public static class LoginResponses
